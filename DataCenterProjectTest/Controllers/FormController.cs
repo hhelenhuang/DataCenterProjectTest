88f16e2822c0091c8042c559d60829adf03a357e
hhelenhuang <ndmchelenhuang@gmail.com>
2022-12-08 22:31:08 +0800

    加入專案檔案。

---
+++ DataCenterProjectTest/Controllers/FormController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataCenterProjectTest.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+    }
+}
